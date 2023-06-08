199825349be9294f83cd610f2db851445ececfc5
Vasilis Gionis <56933592+trelosionikos@users.noreply.github.com>
2023-06-08 23:14:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from flask import Flask
-from flask import render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def Login():
-    return render_template("templ.html")
-  
-@app.route('/name')
-def hello_name(name):
-  return f'Hello {name}!\n'
-
-if __name__ == '__main__':
-   app.run(debug=True)
